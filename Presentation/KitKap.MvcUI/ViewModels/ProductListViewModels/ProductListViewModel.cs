8fe3edea2ba6c52318f06a7a191e6a550fc5260c
Yunus Emre <yunusemre0501@gmail.com>
2025-04-23 21:35:16 +0300

    Ürün sayfası düzeltildi

---
+++ Presentation/KitKap.MvcUI/ViewModels/ProductListViewModels/ProductListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Kitkap.Entity.Entities;
+
+namespace KitKap.MvcUI.ViewModels.ProductListViewModels
+{
+    public class ProductListViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string OwnerId { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public ProductStatus Status { get; set; }
+        public Category Category { get; set; }
+        public ICollection<ProductImage> ProductImages { get; set; }
+        public enum ProductStatus
+        {
+            OutOfStock = 0,
+            InStock = 1,
+            Discontinued = 2
+        }
+    }
+}
